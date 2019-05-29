b4d7a8b9a1348e71a1ee662b2cb4d5f3cff57e01
czy <776103332@qq.com>
2019-05-29 18:29:35 +0800

    整合locgback

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author  Czy on 2019/5/28.
  */
+@MapperScan("com.example.demo.mapper")
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class);
+        System.out.println("----------启动成功----------");
     }
 }
